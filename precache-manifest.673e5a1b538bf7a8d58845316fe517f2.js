b37faa24166babd81408fd948022fdd600cb8d47
崔海峰 <cuihaifeng@adeMacBook-Pro-2.local>
2018-12-19 19:59:31 +0800

    Updates

---
+++ precache-manifest.673e5a1b538bf7a8d58845316fe517f2.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a8691c01a563050fa4",
+    "url": "static/js/blog.19a8691.js"
+  },
+  {
+    "revision": "1f189b47b5512245a28c",
+    "url": "static/css/styles.1f189b4.async.css"
+  },
+  {
+    "revision": "1f189b47b5512245a28c",
+    "url": "static/asyncjs/styles.1f189b4.js"
+  },
+  {
+    "revision": "717131aa5f18951288b6",
+    "url": "static/asyncjs/VUE.skill.717131a.js"
+  },
+  {
+    "revision": "4ba554b7dc8a82ad393c",
+    "url": "static/asyncjs/VUE.plugin.4ba554b.js"
+  },
+  {
+    "revision": "2466740a8f006e2c3ea3",
+    "url": "static/asyncjs/CSS.pageTemplate.2466740.js"
+  },
+  {
+    "url": "sprite.51f2843.svg"
+  }
+];
