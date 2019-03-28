8c8121de790861dfbfea46f5b9049b7850d4a5d4
崔海峰 <cuihaifeng@qutoutiao.net>
2019-03-28 11:38:33 +0800

    feat(*): 添加静态资源:100:

--- build/publish.js
+++ build/publish.js
@@ -1,10 +1,21 @@
 const ghpages = require('gh-pages');
 const chalk = require('chalk');
+const path = require('path')
+const copy = require('copy-template-dir')
 
-ghpages.publish('dist', {
-  branch: 'master',
-  repo: 'https://github.com/13916253446/13916253446.github.io',
-  add: true
-}, function () {
-  console.log(chalk.blue(`\n站点发布成功\n`))
-});
+const assetsDir = path.resolve(__dirname, '../assets')
+const targetDir = path.resolve(__dirname, '../dist')
+
+copy(assetsDir, targetDir, { name: '崔海峰' }, err => {
+  if (!err) {
+    ghpages.publish('dist', {
+      branch: 'master',
+      repo: 'https://github.com/13916253446/13916253446.github.io',
+      add: true
+    }, function () {
+      console.log(chalk.blue(`\n站点发布成功\n`))
+    });
+  } else {
+    console.log(err)
+  }
+})
